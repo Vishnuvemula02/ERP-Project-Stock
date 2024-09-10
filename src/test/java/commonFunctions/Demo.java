f596a3eb384b34a4837850ae5743d6800c06e85e
Dell <Dell@DESKTOP-EV76MD6>
2024-09-10 13:42:58 +0530

    new3

---
+++ src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,29 @@
+package commonFunctions;
+
+public class Demo {
+
+	public static void add() {
+		int a=88,b=77,c;
+		c=a+b;
+	System.out.println(c);
+	
+	}
+	public static void sub() {
+		int a=88,b=77,c;
+		c=a-b;
+	System.out.println(c);
+	
+	}
+	public static void mut() {
+		int a=88,b=77,c;
+		c=a*b;
+	System.out.println(c);
+	
+	}
+	public static void div() {
+		int a=88,b=77,c;
+		c=a/b;
+	System.out.println(c);
+	
+	}
+}
